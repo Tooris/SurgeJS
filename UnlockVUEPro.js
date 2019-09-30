f7739b8f17763cb496baeea4b5b873d4cbdb634d
LimHo <55996925+Tooris@users.noreply.github.com>
2019-10-01 04:17:05 +0800

    Delete UnlockVUEPro.js

--- UnlockVUEPro.js
+++
@@ -1,8 +0,0 @@
-let url = $request.url;
-let body = JSON.parse($response.body);
-let isPremium = '/api/v1/users/self';
-if(url.index0f(isPremium) != true)
-{
-	body.data.isPremium = true;
-}
-$done({body});
